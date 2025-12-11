92d0aa9bb91ce51c1ba04c14d912b4aaecb60ff5
DikiYLexa <dubovan36@gmail.com>
2025-12-11 14:30:11 +0500

    Получение печеньки авторизации. Винокуров

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -29,8 +30,31 @@ namespace HttpNewsPAT
                
             }
             Console.Read();
-            
-            
+        }
+        public static void SingIn(string login, string password)
+        {
+            string Url = "";
+
+            Debug.WriteLine($"Выполняем запрос: {Url}");
+
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(Url);
+            Request.Method = "POST";
+            Request.ContentType = "application/x-www-from-urlencoded";
+            Request.CookieContainer = new CookieContainer();
+            byte[] Data = Encoding.ASCII.GetBytes($"login ={login}&password={password}");
+            Request.ContentLength = Data.Length;
+
+            using (Stream stream = Request.GetRequestStream())
+            {
+                stream.Write(Data, 0, Data.Length);
+            }
+            using (HttpWebResponse response = (HttpWebResponse)Request.GetResponse())
+            {
+                Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+
+                string ResponseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(ResponseFromServer);
+            }
         }
     }
 }
